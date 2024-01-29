8e7308857a7e239fc2685f6caaec75f017278fd4
vigneshkannan52 <vigneshkannan52@gmail.com>
2024-01-29 16:48:42 +0530

    step3

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1706526958/wp-content/themes/snapster/aheto/team-member/assets/js/snapster_layout1.js
@@ -0,0 +1,18 @@
+// ;(function ($) {
+//
+//     'use strict';
+//
+//     if ($('.aheto-team-member__contact').length) {
+//         let heightBlock = parseInt($(".aheto-team-member__contact").css("height"));
+//         let heightNetworks = $( ".aheto-team-member__contact" ).find(".aheto-team-member__networks");
+//
+//         heightNetworks.css({
+//             minWidth: heightBlock,
+//         });
+//     }
+//
+//
+//
+//
+//
+// })(jQuery, window, document);
